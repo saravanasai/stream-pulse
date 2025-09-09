e6e40ab8d0fa3d30edb1c896456926559db8f568
saravanasai <saravanasai002@gmail.com>
2025-09-09 21:06:26 +0530

    Added basic configuration

--- config/stream-pulse.php
+++ config/stream-pulse.php
@@ -2,5 +2,30 @@
 
 // config for StreamPulse/StreamPulse
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Stream Driver
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default stream driver that will be used for
+    | event streaming. Supported drivers: "redis", "null"
+    |
+    */
+    'default' => env('STREAMPULSE_DRIVER', 'redis'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Stream Drivers
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the stream drivers for your application.
+    | Available drivers: "redis"
+    |
+    */
+    'drivers' => [
+        'redis' => [
+            'connection' => env('REDIS_CONNECTION', 'default'),
+            'stream_prefix' => 'streampulse:',
+        ],
+    ],
 ];
